fc335b0833a3ee6d9a0b131a9bc798a191d3e1b7
iTheCode <ithecode.ica@gmail.com>
2018-02-12 20:19:18 -0500

    Change Backend Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -41,6 +41,18 @@ class DashboardController extends BaseController
 		$atenciones_año_anterior = count($atendidos_año_anterior);
 		$porcentaje_antenciones_mes = number_format((100*$atenciones_mes)/$atenciones_año_anterior,2);
 
+		$emergencias_mes = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('MONTH(authorizations.date) = ? AND YEAR(authorizations.date) '), [$month, $year])->where('ct.code', '6')->get());
+		$emergencias_año_anterior = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('YEAR(date) '), [$year-1])->where('ct.code', '6')->get());
+		$porcentaje_emergencias_mes = number_format((100*$emergencias_mes )/$emergencias_año_anterior,2);
+
+		$pacientes_nuevos_mes = count(Patient::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get());
+		$pacientes_año_anterior = count(Patient::where(DB::raw('YEAR(created_at) '), [$year-1])->get());
+		$porcentaje_pacientes_mes = number_format((100*$pacientes_nuevos_mes )/$pacientes_año_anterior,2);
+
+
+		$pay_documents_mes = count(PayDocument::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get()) + count(PayEDocument::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get());
+		$pay_documents_año_anterior = count(PayDocument::where(DB::raw('YEAR(created_at) '), [$year-1])->get()) + count(PayEDocument::where(DB::raw('YEAR(created_at)'), [$year-1])->get()); 
+		$porcentaje_pay_documents_mes = number_format((100*$pay_documents_mes)/$pay_documents_año_anterior,2);
 
 		$atenciones_globales = Authorization::select('id', 'created_at')->where(DB::raw('YEAR(created_at)'), [$year])->get()->groupBy(function($val) {return Carbon::parse($val->created_at)->format('m');});
 
@@ -58,19 +70,7 @@ class DashboardController extends BaseController
 			$month = $meses[$key];
 			$atenciones_globales_last[$month] = array("count" => count($value));
 		}
-		$emergencias_mes = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('MONTH(authorizations.date) = ? AND YEAR(authorizations.date) '), [$month, $year])->where('ct.code', '6')->get());
-		$emergencias_año_anterior = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('YEAR(date) '), [$year-1])->where('ct.code', '6')->get());
-		$porcentaje_emergencias_mes = number_format((100*$emergencias_mes )/$emergencias_año_anterior,2);
 
-		$pacientes_nuevos_mes = count(Patient::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get());
-		$pacientes_año_anterior = count(Patient::where(DB::raw('YEAR(created_at) '), [$year-1])->get());
-		$porcentaje_pacientes_mes = number_format((100*$pacientes_nuevos_mes )/$pacientes_año_anterior,2);
-
-
-		$pay_documents_mes = count(PayDocument::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get()) + count(PayEDocument::where(DB::raw('MONTH(created_at) = ? AND YEAR(created_at) '), [$month, $year])->get());
-		$pay_documents_año_anterior = count(PayDocument::where(DB::raw('YEAR(created_at) '), [$year-1])->get()) + count(PayEDocument::where(DB::raw('YEAR(created_at)'), [$year-1])->get()); 
-		$porcentaje_pay_documents_mes = number_format((100*$pay_documents_mes)/$pay_documents_año_anterior,2);
-		//$pay_documents_mes = $porcentaje_pay_documents_mes = 0;
 		return view('dashboard', ['system_name' => 'CSLuren', 'this_year' => date('Y'), 'user' => $name, 'position' => $position, 'atenciones_mes' => $atenciones_mes, 'porcentaje_antenciones_mes' => $porcentaje_antenciones_mes, 'emergencias_mes' => $emergencias_mes, 'porcentaje_emergencias_mes' => $porcentaje_emergencias_mes, 'pacientes_nuevos_mes' => $pacientes_nuevos_mes, 'porcentaje_pacientes_mes' => $porcentaje_pacientes_mes, 'pay_documents_mes' => $pay_documents_mes, 'porcentaje_pay_documents_mes' => $porcentaje_pay_documents_mes, 'chart_actual' => $atenciones_globales, 'chart_pasado' => $atenciones_globales_last]);
 	}
 	
