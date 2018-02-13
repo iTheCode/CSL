39c661187036e8586b9d92634a733e060adaa4fc
iTheCode <ithecode.ica@gmail.com>
2018-02-12 19:57:26 -0500

    Last Change Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -42,37 +42,22 @@ class DashboardController extends BaseController
 		$porcentaje_antenciones_mes = number_format((100*$atenciones_mes)/$atenciones_año_anterior,2);
 
 
-		$atenciones_globales = Authorization::select('id', 'created_at')->where(DB::raw('YEAR(date) '), [$year])->get()->groupBy(function($val) {return Carbon::parse($val->created_at)->format('m');});
+		$atenciones_globales = Authorization::select('id', 'created_at')->where(DB::raw('YEAR(created_at)'), [$year])->get()->groupBy(function($val) {return Carbon::parse($val->created_at)->format('m');});
 
+		$atenciones_globales_last = Authorization::select('id', 'created_at')->where(DB::raw('YEAR(created_at)'), [$year - 1])->get()->groupBy(function($val) {return Carbon::parse($val->created_at)->format('m');});
 		$meses = array("NULL","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 		foreach ($atenciones_globales as $key => $value) {
-			$quantity_month = 0;
-			/*foreach ($value as $response) {
-				$quantity = 0;
-				if(isset($response->insuredservices))
-					foreach($response->insuredservices as $i){
-						$quantity = $quantity + $i->initial_amount;
-					}
-				if(isset($response->insuredpharmacies))
-					foreach($response->insuredpharmacies as $i){
-						$quantity = $quantity + $i->initial_amount;
-					}
-
-				if(isset($response->particularservices))
-					foreach($response->particularservices as $i){
-						$quantity = $quantity + $i->initial_amount;
-					}
-				if(isset($response->coverage))
-					$quantity = $quantity + $response->coverage->cop_fijo;
-
-				$quantity_month = $quantity_month + $quantity;
-			}*/
 			unset($atenciones_globales[$key]);
 			$key = intval($key);
 			$month = $meses[$key];
 			$atenciones_globales[$month] = array("count" => count($value));
 		}
-		dd($atenciones_globales);
+		foreach ($atenciones_globales_last as $key => $value) {
+			unset($atenciones_globales_last[$key]);
+			$key = intval($key);
+			$month = $meses[$key];
+			$atenciones_globales_last[$month] = array("count" => count($value));
+		}
 		$emergencias_mes = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('MONTH(authorizations.date) = ? AND YEAR(authorizations.date) '), [$month, $year])->where('ct.code', '6')->get());
 		$emergencias_año_anterior = count(Authorization::join("coverages AS c", "c.authorization_id", "=", "authorizations.id")->join("sub_coverage_types AS sub", "c.sub_coverage_type_id", "=", "sub.id")->join("coverage_types AS ct", "ct.id", "=", "sub.coverage_type_id")->where(DB::raw('YEAR(date) '), [$year-1])->where('ct.code', '6')->get());
 		$porcentaje_emergencias_mes = number_format((100*$emergencias_mes )/$emergencias_año_anterior,2);
@@ -86,7 +71,7 @@ class DashboardController extends BaseController
 		$pay_documents_año_anterior = count(PayDocument::where(DB::raw('YEAR(created_at) '), [$year-1])->get()) + count(PayEDocument::where(DB::raw('YEAR(created_at)'), [$year-1])->get()); 
 		$porcentaje_pay_documents_mes = number_format((100*$pay_documents_mes)/$pay_documents_año_anterior,2);
 		//$pay_documents_mes = $porcentaje_pay_documents_mes = 0;
-		return view('dashboard', ['system_name' => 'CSLuren', 'this_year' => date('Y'), 'user' => $name, 'position' => $position, 'atenciones_mes' => $atenciones_mes, 'porcentaje_antenciones_mes' => $porcentaje_antenciones_mes, 'emergencias_mes' => $emergencias_mes, 'porcentaje_emergencias_mes' => $porcentaje_emergencias_mes, 'pacientes_nuevos_mes' => $pacientes_nuevos_mes, 'porcentaje_pacientes_mes' => $porcentaje_pacientes_mes, 'pay_documents_mes' => $pay_documents_mes, 'porcentaje_pay_documents_mes' => $porcentaje_pay_documents_mes]);
+		return view('dashboard', ['system_name' => 'CSLuren', 'this_year' => date('Y'), 'user' => $name, 'position' => $position, 'atenciones_mes' => $atenciones_mes, 'porcentaje_antenciones_mes' => $porcentaje_antenciones_mes, 'emergencias_mes' => $emergencias_mes, 'porcentaje_emergencias_mes' => $porcentaje_emergencias_mes, 'pacientes_nuevos_mes' => $pacientes_nuevos_mes, 'porcentaje_pacientes_mes' => $porcentaje_pacientes_mes, 'pay_documents_mes' => $pay_documents_mes, 'porcentaje_pay_documents_mes' => $porcentaje_pay_documents_mes, 'chart_actual' => $atenciones_globales, 'chart_pasado' => $atenciones_globales_last]);
 	}
 	
 }
