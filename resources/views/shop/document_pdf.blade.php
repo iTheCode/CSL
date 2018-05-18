985421ecf05132dd38fb55ae4cadc6079ab92e9e
iTheCode <ithecode.ica@gmail.com>
2018-05-18 14:36:37 -0500

    changes sunat resolution

--- resources/views/shop/document_pdf.blade.php
+++ resources/views/shop/document_pdf.blade.php
@@ -446,7 +446,7 @@ $descuentoP = Helpers::number_format_sunat((100*$descuento)/$final_amount,0);
         <table cellspacing="0" style="width: 8em;">
             <tr>
                 <td style=" padding-right: -60px ;font-size:0.7em;text-align:right;">
-                    Autorización Resolución N° 203-2015/SUNAT
+                    Autorizado Mediante Resolución:<br> N° 203-2015/SUNAT
                 </td>
             </tr>
             <tr>
